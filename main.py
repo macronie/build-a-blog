f30f6f26e1ba3964d0cb76d9fc3695e379c79913
macronie <poornima08@yahoo.co.in>
2018-01-15 21:42:27 -0600

    done

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, redirect, render_template, url_for, session
 from flask_sqlalchemy import SQLAlchemy
-
+import string, cgi
 app = Flask(__name__)
 app.config['DEBUG'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:password@localhost:8889/build-a-blog'
@@ -21,70 +21,83 @@ class Blog(db.Model):
         self.completed = False
 
 #use this part to add new blog list
-@app.route('/', methods=['POST', 'GET'])
+@app.route('/')
 def index():
-    title_name = "" 
-    if request.method == 'POST':
-        title_name = request.form['title']
-        blog_content = request.form['blog-content']
-        
-        new_blog = Blog(title_name, blog_content)
-        db.session.add(new_blog )
-        db.session.commit()
+    return redirect(url_for('blog'))
 
+#use this part to add new blog list
+@app.route('/blog')
+def blog():
+    blog_id = request.args.get('id')
+    if blog_id != None:
+        tasks = Blog.query.filter_by(id = blog_id).all()
+        #return '404 - hI Template not found'
+        #url_text = urlencode("/blogcontent.html?blogid=")
+        if tasks != None:
+            return render_template('/blogcontent.html', tasks=tasks)
+        else:
+            return '<h1>Blog doesnt exist for the ID </h>'
+
+    else:
         tasks = Blog.query.filter_by(completed=False).all()
         #completed_tasks = Blog.query.filter_by(completed=True).all()
-        return render_template('blog-listing.html', tasks=tasks)
-
-    if request.method == 'GET':
-        tasks = Blog.query.filter_by(completed=False).all()
-        #completed_tasks = Blog.query.filter_by(completed=True).all()
-        return render_template('blog-listing.html', tasks=tasks)
+        return render_template('blog.html', tasks=tasks)
 
 @app.route('/new_blog', methods=['POST', 'GET'])
 def new_blog():
+    
     title_name = "" 
+    title_name = cgi.escape(title_name, quote=True)
+    add_error=""
+    add_error = cgi.escape(add_error, quote=True)
+    blog_content=""
+    blog_content = cgi.escape(blog_content, quote=True)
     if request.method == 'POST':
         title_name = request.form['title']
         blog_content = request.form['blog-content']
-        
-        new_blog = Blog(title_name, blog_content)
-        db.session.add(new_blog )
-        db.session.commit()
+        if title_name and blog_content:
+            new_blog = Blog(title_name, blog_content)
+            db.session.add(new_blog )
+            db.session.commit()
 
-        tasks = Blog.query.filter_by(completed=False).all()
-        completed_tasks = Blog.query.filter_by(completed=True).all()
+            tasks = Blog.query.filter_by(completed=False).all()
+            completed_tasks = Blog.query.filter_by(completed=True).all()
+            
+            #return render_template('blog-listing.html', tasks=tasks)
+            #form_value = request.form['param_name']
+            obj = Blog.query.order_by(Blog.id.desc()).first()
         
-        #return render_template('blog-listing.html', tasks=tasks)
-        #form_value = request.form['param_name']
-        obj = Blog.query.order_by(Blog.id.desc()).first()
-       
-        most_recent_idx=obj.id
-        return redirect(url_for('blogcontent',id=most_recent_idx))
+            most_recent_idx=obj.id
+            return redirect(url_for('blog',id=most_recent_idx))
+        else:
+            if(title_name):
+                add_error = 'Body cannot be blank'
+            else:
+                add_error = 'Title cannot be blank'
+            return render_template('new_post.html', title=request.form['title'], blog=request.form['blog-content'], error=add_error)
+
     if request.method == 'GET':
         
         #completed_tasks = Blog.query.filter_by(completed=True).all()
         return render_template('new_post.html')
+        
+#step2(gets username/user id from the link)    
+@app.route('/blog1')
+def blog1():
+    #handles id part
+    blog_id = request.args.get('id')
+    tasks = Blog.query.filter_by(id = blog_id).all()
+    #return '404 - hI Template not found'
+    #url_text = urlencode("/blogcontent.html?blogid=")
+    return render_template('/blogcontent.html', tasks=tasks)
 
 @app.route('/blogcontent')
 def blogcontent():
-    #return 'User %s' % blog_id
-    #blog_id = int(request.GET.get('task.id')) 
-    #return '404 - Template not found'
-    #if request.method == 'GET':
         blog_id=request.args.get('id','')
         tasks = Blog.query.filter_by(id = blog_id).all()
         #return '404 - hI Template not found'
         return render_template('/blogcontent.html', tasks=tasks)
 
-@app.route('/blog-listing')
-def bloglisting():
-    return redirect('/blog-listing')
-
-@app.route('/bloglistinglink', methods=['GET', 'POST'])
-def bloglistinglink():
-    tasks = Blog.query.filter_by(completed=False).all()
-    return render_template('/blog-listing.html', tasks=tasks)
 
 
 
